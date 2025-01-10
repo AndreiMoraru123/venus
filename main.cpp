8b4017edf4d798393e0881b97daece0a85e75e20
AndreiMoraru123 <andreimoraru1999123@gmail.com>
2025-01-10 09:46:54 +0200

    play around with clangd and concepts

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
+#include <concepts>
 #include <print>
 #include <tuple>
 
-consteval auto Vector(auto x, auto y) {
+template <typename T>
+concept Numeric = std::integral<T> || std::floating_point<T>;
+
+consteval auto Vector(Numeric auto x, Numeric auto y) {
   auto getX = [x] consteval { return x; };
   auto getY = [y] consteval { return y; };
 
@@ -15,7 +19,7 @@ consteval auto Vector(auto x, auto y) {
 
 auto main() -> int {
   constexpr auto vector1 = Vector(1, 2);
-  constexpr auto vector2 = Vector(2, 4);
+  constexpr auto vector2 = Vector(3.5, 4.5);
 
   constexpr auto v1Add = std::get<2>(vector1);
 
