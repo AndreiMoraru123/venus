4b3ad4cbc4c77fd597529181a29163f19cb0ab78
AndreiMoraru123 <andreimoraru1999123@gmail.com>
2025-01-10 10:07:24 +0200

    play around with modules

--- main.cpp
+++ main.cpp
@@ -1,25 +1,9 @@
-#include <concepts>
+import vector;
 #include <print>
-#include <tuple>
-
-template <typename T>
-concept Numeric = std::integral<T> || std::floating_point<T>;
-
-consteval auto Vector(Numeric auto x, Numeric auto y) {
-  auto getX = [x] consteval { return x; };
-  auto getY = [y] consteval { return y; };
-
-  auto add = [x, y](auto other) consteval {
-    const auto [otherX, otherY, _] = other;
-    return Vector(x + otherX(), y + otherY());
-  };
-
-  return std::make_tuple(getX, getY, add);
-}
 
 auto main() -> int {
   constexpr auto vector1 = Vector(1, 2);
-  constexpr auto vector2 = Vector(3.5, 4.5);
+  constexpr auto vector2 = Vector(2.5, 3.5);
 
   constexpr auto v1Add = std::get<2>(vector1);
 
