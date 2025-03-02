eecdcdb48ccbed417bac946148d5903cadb84c2e
AndreiMoraru123 <andreimoraru1999123@gmail.com>
2025-03-02 12:30:45 +0200

    identity trait

--- venus/traits.hpp
+++ venus/traits.hpp
@@ -1,5 +1,12 @@
 
 #include <type_traits>
 
+namespace venus {
+
 template <typename T>
 using RemoveConstRef = std::remove_const_t<std::remove_reference_t<T>>;
+
+template <typename T> struct Identity_ {
+  using type = T;
+};
+} // namespace venus
