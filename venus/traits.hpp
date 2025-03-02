1ebf2bb357c642011bd2d2dcaba37e3e216bbdde
AndreiMoraru123 <andreimoraru1999123@gmail.com>
2025-03-02 13:12:38 +0200

    and trait

--- venus/traits.hpp
+++ venus/traits.hpp
@@ -9,4 +9,9 @@ using RemoveConstRef = std::remove_const_t<std::remove_reference_t<T>>;
 template <typename T> struct Identity_ {
   using type = T;
 };
+
+template <bool curr, typename TNext> static constexpr bool AndValue = false;
+
+template <typename TNext>
+static constexpr bool AndValue<true, TNext> = TNext::value;
 } // namespace venus
