bfee532d91531d48ac87de7bc034ab04f67cb7c9
AndreiMoraru123 <andreimoraru1999123@gmail.com>
2025-04-13 17:18:14 +0300

    contig memo

---
+++ core/memory/contiguous_memory.hpp
@@ -0,0 +1,40 @@
+#include "../traits.hpp"
+#include "allocators.hpp"
+#include <cassert>
+#include <cstddef>
+#include <memory>
+#include <type_traits>
+
+namespace venus {
+
+template <typename TElem, typename TDevice> class ContiguousMemory {
+  static_assert(std::is_same_v<RemoveConstRef<TElem>, TElem>);
+  using ElementType = TElem;
+
+public:
+  explicit ContiguousMemory(std::size_t p_size)
+      : m_mem(Allocator<TDevice>::template Allocate<ElementType>(p_size)),
+        m_size(p_size) {}
+
+  auto Shift(size_t pos) const {
+    assert(pos < m_size);
+    return ContiguousMemory(
+        std::shared_ptr<ElementType>(m_mem, m_mem.get() + pos), m_size - pos);
+  }
+
+  auto RawMemory() const { return m_mem.get(); }
+  bool IsShared() const { return not m_mem.unique(); }
+  std::size_t Size() const { return m_size; }
+
+  bool operator==(const ContiguousMemory &val) const {
+    return (m_mem == val.m_mem) and (m_size == val.m_size);
+  }
+
+private:
+  ContiguousMemory(std::shared_ptr<ElementType> ptr, std::size_t sz)
+      : m_mem(std::move(ptr)), m_size(sz) {}
+  std::shared_ptr<ElementType> m_mem;
+  std::size_t m_size;
+};
+
+} // namespace venus
---
+++ test/memory/test_contiguous_memory.cpp
@@ -0,0 +1,47 @@
+#include <catch2/catch_test_macros.hpp>
+
+#include <core/memory/contiguous_memory.hpp>
+
+using namespace venus;
+
+TEST_CASE("ContiguousMemory basics", "[memory]") {
+  SECTION("Construction") {
+    constexpr std::size_t size = 8;
+    ContiguousMemory<float *, Device::CPU> memo(size);
+
+    REQUIRE(memo.Size() == size);
+    REQUIRE(memo.RawMemory() != nullptr);
+    REQUIRE_FALSE(memo.IsShared());
+  }
+
+  SECTION("Equality Ops") {
+    ContiguousMemory<float *, Device::CPU> memo1(10);
+    ContiguousMemory<float *, Device::CPU> memo2(10);
+
+    REQUIRE(memo1 == memo1);
+    REQUIRE(memo1 != memo2);
+  }
+
+  SECTION("Shift Op") {
+    ContiguousMemory<char, Device::CPU> memo(100);
+    auto shifted = memo.Shift(20);
+
+    REQUIRE(shifted.Size() == 80);
+    REQUIRE(shifted.RawMemory() == memo.RawMemory() + 20);
+    REQUIRE(shifted.IsShared());
+    REQUIRE(memo.IsShared());
+  }
+
+  SECTION("Shared state") {
+    ContiguousMemory<char, Device::CPU> memo(50);
+
+    REQUIRE_FALSE(memo.IsShared());
+    {
+      auto copy = memo;
+      REQUIRE(memo.IsShared());
+      REQUIRE(copy.IsShared());
+      REQUIRE(memo == copy);
+    }
+    REQUIRE_FALSE(memo.IsShared());
+  }
+}
