505e6e26c553da5e18e5918bea2de4a5928ce3ee
AndreiMoraru123 <andreimoraru1999123@gmail.com>
2025-05-20 20:14:07 +0300

    tag todo in allocator

--- core/memory/allocators.hpp
+++ core/memory/allocators.hpp
@@ -8,7 +8,7 @@ namespace venus {
 template <typename TDevice> struct Allocator;
 
 template <> struct Allocator<Device::CPU> {
-  // TODO: Replace with a memory pool
+  // TODO: Replace with a memory pool (mempool)
   template <typename TElem>
   static std::shared_ptr<TElem> Allocate(std::size_t p_elemSize) {
     return std::shared_ptr<TElem>(new TElem[p_elemSize],
