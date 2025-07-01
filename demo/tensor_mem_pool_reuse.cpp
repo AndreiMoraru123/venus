3e10904a9c5c65c750e2bff07075ed9924969037
AndreiMoraru123 <andreimoraru1999123@gmail.com>
2025-07-01 11:05:11 +0300

    tensor mem pool reuse demo

---
+++ demo/tensor_mem_pool_reuse.cpp
@@ -0,0 +1,23 @@
+#include "core/tensor/tensor.hpp"
+#include <print>
+
+using namespace venus;
+
+auto main() -> int {
+  {
+    auto tensor = Tensor<int, Device::CPU, 2>(3, 3);
+    std::ranges::iota(tensor, 1); // 1, 2, 3, 4, 5, 6, 7, 8, 9
+    for (auto el : tensor) {
+      std::print("{} ", static_cast<int>(el));
+    }
+  }
+
+  std::print("\n");
+
+  {
+    auto tensor2 = Tensor<int, Device::CPU, 2>(3, 3);
+    for (auto el : tensor2) {
+      std::print("{} ", static_cast<int>(el)); // this should be all 0s
+    }
+  }
+}
