d8f8439812fe24850f6f88ae9bd0ef297a57bb0b
AndreiMoraru123 <andreimoraru1999123@gmail.com>
2025-07-05 22:54:13 +0300

    string tensor direct print

--- demo/string_tensor.cpp
+++ demo/string_tensor.cpp
@@ -9,6 +9,7 @@ using namespace venus;
 auto main() -> int {
   auto tensor = Tensor<std::string, Device::CPU, 1>(3);
   std::ranges::fill(tensor, "hello");
+  std::println("{}", tensor); // venus::Tensor([hello, hello, hello], shape=(3))
 
   for (auto el : tensor) {
     std::print("{} ", el); // hello hello hello
