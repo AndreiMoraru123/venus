e6f113097c2eaf3f32c5232a5e1723f9fdde6726
AndreiMoraru123 <andreimoraru1999123@gmail.com>
2025-07-07 12:13:53 +0300

    mem pool demo

---
+++ demo/mem_pool.cpp
@@ -0,0 +1,38 @@
+
+#include "core/memory/device.hpp"
+#include "core/tensor/tensor.hpp"
+#include <algorithm>
+#include <cassert>
+#include <print>
+
+using namespace venus;
+
+// clang-format off
+auto main() -> int {
+  {
+    auto tensor = Tensor<float, Device::CPU, 2>(2, 3);
+    assert(tensor.HasUniqueMemory());
+
+    std::ranges::fill(tensor, 10.0f);
+    std::println("{}", tensor); // venus::Tensor([10.00, 10.00, 10.00, 10.00, 10.00, 10.00], shape=(2, 3))
+
+    auto tensor2 = Tensor<float, Device::CPU, 2>(2, 3);
+    assert(tensor2.HasUniqueMemory());
+
+    std::println("{}", tensor); // venus::Tensor([0.00, 0.00, 0.00, 0.00, 0.00, 0.00], shape=(2, 3))
+  }
+  {
+    const auto memo = ContiguousMemory<float, Device::CPU>(6);
+
+    auto tensor = Tensor<float, Device::CPU, 2>(memo, Shape(2, 3));
+    assert(not tensor.HasUniqueMemory());
+
+    std::ranges::fill(tensor, 5.0f);
+    std::println("{}", tensor); // venus::Tensor([5.00, 5.00, 5.00, 5.00, 5.00, 5.00], shape=(2, 3))
+
+    auto tensor2 = Tensor<float, Device::CPU, 2>(memo, Shape(2, 3));
+    assert(not tensor2.HasUniqueMemory());
+
+    std::println("{}", tensor2); // venus::Tensor([5.00, 5.00, 5.00, 5.00, 5.00, 5.00], shape=(2, 3))
+  }
+}
