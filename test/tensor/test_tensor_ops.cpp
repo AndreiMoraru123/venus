1d33ec71cad132f2531fbac2cf2a80acef0d0c63
AndreiMoraru123 <andreimoraru1999123@gmail.com>
2025-09-13 17:22:09 +0300

    rewrite eager ops test

--- test/tensor/test_tensor_ops.cpp
+++ test/tensor/test_tensor_ops.cpp
@@ -1,39 +1,45 @@
 #include "core/memory/device.hpp"
 #include <cassert>
 #include <catch2/catch_test_macros.hpp>
+#include <catch2/generators/catch_generators.hpp>
+#include <functional>
 #include <print>
 
 #include <cmath>
 #include <core/tensor/tensor.hpp>
 #include <numeric>
+#include <tuple>
 
 using namespace venus;
 
 TEST_CASE("Tensor Ops", "[tensor][ops]") {
-  SECTION("Scalar Addition") {
+  SECTION("Scalar Ops") {
     auto x = Tensor<int, Device::CPU, 0>(5);
     auto y = Tensor<float, Device::CPU, 0>(2.5f);
-    auto z = x + y;
 
-    STATIC_REQUIRE(
-        std::is_same_v<decltype(z)::ElementType, float>); // type promotion
-    REQUIRE(z.Value() == 7.5f);
+    using Op = std::function<decltype(x + y)(decltype(x), decltype(y))>;
+
+    auto [name, op, expected] = GENERATE(
+        std::make_tuple("Addition", Op([](auto a, auto b) { return a + b; }),
+                        7.5f),
+        std::make_tuple("Multiplication",
+                        Op([](auto a, auto b) { return a * b; }), 12.5f),
+        std::make_tuple("Subtraction", Op([](auto a, auto b) { return a - b; }),
+                        2.5f),
+        std::make_tuple("Division", Op([](auto a, auto b) { return a / b; }),
+                        2.0f));
+
+    DYNAMIC_SECTION("Scalar " << name) {
+      auto result = op(x, y);
+
+      STATIC_REQUIRE(std::is_same_v<decltype(result)::ElementType, float>);
+      REQUIRE(result.Value() == expected);
+    }
   }
 
-  SECTION("Scalar Multiplication") {
-    auto x = Tensor<int, Device::CPU, 0>(5);
-    auto y = Tensor<float, Device::CPU, 0>(2.5f);
-    auto z = x * y;
-
-    STATIC_REQUIRE(
-        std::is_same_v<decltype(z)::ElementType, float>); // type promotion
-    REQUIRE(z.Value() == 12.5f);
-  }
-
-  SECTION("Tensor Addition") {
+  SECTION("Tensor Ops") {
     auto x = Tensor<int, Device::CPU, 2>(3, 3);
     auto y = Tensor<float, Device::CPU, 2>(3, 3);
-    auto expected = Tensor<int, Device::CPU, 2>(3, 3);
 
 #if _cpp_lib_ranges >= 202110L
     std::ranges::iota(x, 1);
@@ -42,49 +48,56 @@ TEST_CASE("Tensor Ops", "[tensor][ops]") {
 #else
     std::iota(x.begin(), x.end(), 1);
     std::iota(y.begin(), y.end(), 1);
-    std::iota(expected.begin(), expected.end(), 1);
 #endif
 
-    std::ranges::transform(expected, expected.begin(),
-                           [](int val) { return val + val; });
-    auto z = x + y;
+    using TensorOp = std::function<decltype(x + y)(decltype(x), decltype(y))>;
+    using ScalarOp = std::function<float(int, float)>;
 
-    STATIC_REQUIRE(
-        std::is_same_v<decltype(z)::ElementType, float>); // type promotion
-    REQUIRE(z.Shape() == x.Shape());
-    REQUIRE(std::ranges::equal(z, expected));
+    auto [name, tensor_op, scalar_op] = GENERATE(
+        std::make_tuple("Addition",
+                        TensorOp([](auto a, auto b) { return a + b; }),
+                        ScalarOp([](int a, float b) { return a + b; })),
+        std::make_tuple("Multiplication",
+                        TensorOp([](auto a, auto b) { return a * b; }),
+                        ScalarOp([](int a, float b) { return a * b; })),
+        std::make_tuple("Subtraction",
+                        TensorOp([](auto a, auto b) { return a - b; }),
+                        ScalarOp([](int a, float b) { return a - b; })),
+        std::make_tuple("Division",
+                        TensorOp([](auto a, auto b) { return a / b; }),
+                        ScalarOp([](int a, float b) { return a / b; })));
+
+    DYNAMIC_SECTION("Tensor " << name) {
+      auto result = tensor_op(x, y);
+      auto expected = Tensor<int, Device::CPU, 2>(3, 3);
+      std::ranges::transform(x, y, expected.begin(),
+                             [&scalar_op](int x_val, float y_val) {
+                               return scalar_op(x_val, y_val);
+                             });
+
+      REQUIRE(result.Shape() == x.Shape());
+      REQUIRE(result.Shape() == y.Shape());
+      REQUIRE(std::ranges::equal(result, expected));
+    }
   }
 
-  SECTION("Tensor Multiplication") {
-    auto x = Tensor<int, Device::CPU, 2>(3, 3);
-    auto y = Tensor<float, Device::CPU, 2>(3, 3);
-    auto expected = Tensor<int, Device::CPU, 2>(3, 3);
-
-#if _cpp_lib_ranges >= 202110L
-    std::ranges::iota(x, 1);
-    std::ranges::iota(y, 1);
-    std::ranges::iota(expected, 1);
-#else
-    std::iota(x.begin(), x.end(), 1);
-    std::iota(y.begin(), y.end(), 1);
-    std::iota(expected.begin(), expected.end(), 1);
-#endif
-
-    std::ranges::transform(expected, expected.begin(),
-                           [](int val) { return val * val; });
-    auto z = x * y;
-
-    STATIC_REQUIRE(
-        std::is_same_v<decltype(z)::ElementType, float>); // type promotion
-    REQUIRE(z.Shape() == x.Shape());
-    REQUIRE(std::ranges::equal(z, expected));
-  }
-
-  SECTION("Tensor Addition (shape mismatch)") {
+  SECTION("Tensor Ops (Shape Mismatch)") {
     auto x = Tensor<int, Device::CPU, 2>(3, 3);
     auto y = Tensor<int, Device::CPU, 2>(2, 2);
 
-    REQUIRE_THROWS_AS(x + y, std::invalid_argument);
+    using Op = std::function<decltype(x + y)(decltype(x), decltype(y))>;
+
+    auto [name, op] = GENERATE(
+        std::make_tuple("Addition", Op([](auto a, auto b) { return a + b; })),
+        std::make_tuple("Multiplication",
+                        Op([](auto a, auto b) { return a * b; })),
+        std::make_tuple("Subtraction",
+                        Op([](auto a, auto b) { return a - b; })),
+        std::make_tuple("Division", Op([](auto a, auto b) { return a / b; })));
+
+    DYNAMIC_SECTION("Tensor " << name << " (Shape Mismatch)") {
+      REQUIRE_THROWS_AS(op(x, y), std::invalid_argument);
+    }
   }
 
   SECTION("Scalar Transform") {
