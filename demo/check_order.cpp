426832a5893a69a95abce73b08f8cc5a6db4598a
AndreiMoraru123 <andreimoraru1999123@gmail.com>
2025-12-13 09:12:33 +0200

    check order demo

---
+++ demo/check_order.cpp
@@ -0,0 +1,14 @@
+#include "core/tensor/tensor.hpp"
+#include <print>
+
+using namespace venus;
+
+auto check_order(auto ints) {
+  return venus::ops::where(venus::ops::sort(ints) != ints);
+}
+
+auto main() -> int {
+  auto ints = Tensor<int, Device::CPU, 1>{5, 2, 4, 3, 1};
+  std::println("Difference at indices: {}",
+               check_order(ints)); // venus::Tensor([0, 0, 2, 3, 4], shape=(5))
+}
