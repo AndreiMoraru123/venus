ff08de4968942fef54974535e1050ebad86018da
AndreiMoraru123 <andreimoraru1999123@gmail.com>
2025-12-17 17:07:19 +0200

    pragma once for policy macros

--- core/policies/policy_macro_begin.hpp
+++ core/policies/policy_macro_begin.hpp
@@ -1,3 +1,5 @@
+#pragma once
+
 #define EnumValuePolicyObj(PolicyName, Ma, Mi, Val)                            \
   struct PolicyName : virtual public Ma {                                      \
     using MinorClass = Ma::Mi##TypeCate;                                       \
--- core/policies/policy_macro_end.hpp
+++ core/policies/policy_macro_end.hpp
@@ -1,3 +1,5 @@
+#pragma once
+
 #undef EnumTypePolicyObj
 #undef ValuePolicyObj
 #undef TypePolicyTemplate
