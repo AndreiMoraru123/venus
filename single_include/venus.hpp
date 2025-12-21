71d99b21393e49b94ed72c7eb4989460555afbb8
AndreiMoraru123 <andreimoraru1999123@gmail.com>
2025-12-21 11:33:45 +0200

    [msvc] std::ranges ops for msvc as well

--- single_include/venus.hpp
+++ single_include/venus.hpp
@@ -3,12 +3,11 @@
 // Auto-generated main header
 
 #if defined(_MSC_VER)
-// Always use range-v3 for MSVC
-#include <range/v3/view/transform.hpp>
-#include <range/v3/view/zip.hpp>
+// MSVC: use std::views
+#include <ranges>
 namespace venus::compat {
-using ranges::views::transform;
-using ranges::views::zip;
+using std::views::transform;
+using std::views::zip;
 } // namespace venus::compat
 #elif __cplusplus >= 202302L
 // C++23: use std::views
