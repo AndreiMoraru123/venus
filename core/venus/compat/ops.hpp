c8d96729604becc905491673cd7d99e07005b5ed
AndreiMoraru123 <andreimoraru1999123@gmail.com>
2025-12-22 06:43:36 +0200

    fix missing import in compat ops

--- core/venus/compat/ops.hpp
+++ core/venus/compat/ops.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #ifdef VENUS_INTERPRETER
 // C++17/20: use range-v3
+#include <range/v3/view/transform.hpp>
 #include <range/v3/view/zip.hpp>
 namespace venus::compat {
 using ranges::views::transform;
