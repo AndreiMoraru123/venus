7669663721e939acb8ad0869e6108a492cfb11cd
AndreiMoraru123 <81184255+AndreiMoraru123@users.noreply.github.com>
2025-12-22 04:44:12 +0000

    Update single header with latest code

--- single_include/venus.hpp
+++ single_include/venus.hpp
@@ -4,6 +4,7 @@
 
 #ifdef VENUS_INTERPRETER
 // C++17/20: use range-v3
+#include <range/v3/view/transform.hpp>
 #include <range/v3/view/zip.hpp>
 namespace venus::compat {
 using ranges::views::transform;
