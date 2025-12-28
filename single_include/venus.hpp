f5070d4c0297717ffca379906b5e9c98cc0a75f2
AndreiMoraru123 <81184255+AndreiMoraru123@users.noreply.github.com>
2025-12-28 02:13:34 +0000

    Update single header with latest code

--- single_include/venus.hpp
+++ single_include/venus.hpp
@@ -1811,7 +1811,10 @@ template <typename... TParameters> struct VarTypeDict {
 
   template <typename... Types> struct Values {
 
-    using Keys = VarTypeDict;
+    static constexpr std::size_t TupleSize =
+        sizeof...(Types) == 0 ? 1 : sizeof...(Types);
+
+    using Tuple = std::array<std::shared_ptr<void>, TupleSize>;
 
     template <typename TKey>
     using ValueType =
@@ -1834,8 +1837,7 @@ template <typename... TParameters> struct VarTypeDict {
     auto operator=(Values &&) -> Values & = default;
     ~Values() = default;
 
-    Values(std::array<std::shared_ptr<void>, sizeof...(Types)> &&input)
-        : m_tuple(std::move(input)) {}
+    Values(Tuple &&input) : m_tuple(std::move(input)) {}
 
     template <typename TTag, typename... TParams>
     void Update(TParams &&...params) {
@@ -1906,9 +1908,7 @@ template <typename... TParameters> struct VarTypeDict {
     }
 
   private:
-    std::array<std::shared_ptr<void>,
-               sizeof...(Types) == 0 ? 1 : sizeof...(Types)>
-        m_tuple{};
+    Tuple m_tuple{};
   };
 
   static auto Create() {
