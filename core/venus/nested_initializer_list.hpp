2d2952c10880db750359bb09c6b1a0979461233c
AndreiMoraru123 <andreimoraru1999123@gmail.com>
2026-01-03 13:27:13 +0200

    update nested_initializer_list code style

--- core/venus/nested_initializer_list.hpp
+++ core/venus/nested_initializer_list.hpp
@@ -3,15 +3,14 @@
 #include <cstddef>
 #include <initializer_list>
 
-template <class T, std::size_t Depth> struct nested_initializer_list {
+template <typename T, std::size_t Depth> struct nested_initializer_list {
   using type = std::initializer_list<
       typename nested_initializer_list<T, Depth - 1>::type>;
 };
 
-template <class T> struct nested_initializer_list<T, 1> {
+template <typename T> struct nested_initializer_list<T, 1> {
   using type = std::initializer_list<T>;
 };
 
-template <class T, std::size_t Depth>
-using nested_initializer_list_t =
-    typename nested_initializer_list<T, Depth>::type;
+template <typename T, std::size_t Depth>
+using nested_initializer_list_t = nested_initializer_list<T, Depth>::type;
