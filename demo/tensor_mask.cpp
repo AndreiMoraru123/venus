b5426fbde03d6aeb2cd507ca4871b84b19725d8c
AndreiMoraru123 <andreimoraru1999123@gmail.com>
2026-02-03 13:22:34 +0200

    rename condition -> mask

--- demo/tensor_mask.cpp
+++ demo/tensor_mask.cpp
@@ -11,8 +11,8 @@ auto main() -> int {
   venus::ops::iota(x, 1); // venus::Tensor([1.00, 2.00, 3.00, 4.00, 5.00, 6.00], shape=(3, 2))
   venus::ops::fill(y, 1); // venus::Tensor([1.00, 1.00, 1.00, 1.00, 1.00, 1.00], shape=(3, 2))
 
-  auto condition = x > 3;
-  std::println("{}", condition); // venus::Tensor([0, 0, 0, 1, 1, 1], shape=(3, 2))
+  auto mask = x > 3;
+  std::println("{}", mask); // venus::Tensor([0, 0, 0, 1, 1, 1], shape=(3, 2))
 
   auto z = venus::ops::where(x > 3);
   std::println("{}", z); // venus::Tensor([0, 0, 0, 3, 4, 5], shape=(3, 2))
