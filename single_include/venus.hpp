6ae196d9eae56d285beaee2c177950e354505bdb
AndreiMoraru123 <81184255+AndreiMoraru123@users.noreply.github.com>
2026-02-03 05:26:25 +0000

    Update single header with latest code

--- single_include/venus.hpp
+++ single_include/venus.hpp
@@ -66,7 +66,7 @@ private:
   struct Deleter {
     Deleter(std::deque<void *> &p_refPool) : m_refPool(p_refPool) {}
     void operator()(void *p_val) const {
-      std::lock_guard<std::mutex> guard(m_mutex);
+      std::scoped_lock<std::mutex> guard(m_mutex);
       m_refPool.push_back(p_val);
     }
 
@@ -89,7 +89,7 @@ public:
       p_elemSize = ((p_elemSize / BlockSize) + 1) * BlockSize;
     }
 
-    std::lock_guard<std::mutex> guard(m_mutex);
+    std::scoped_lock<std::mutex> guard(m_mutex);
 
     T *raw_buf = nullptr;
     auto &slot = m_pool.memBuffer[p_elemSize];
