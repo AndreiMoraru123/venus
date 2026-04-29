c3e5dc32334d28efb8ca06b6a5bc106e933d8d0d
AndreiMoraru123 <andreimoraru1999123@gmail.com>
2026-04-30 00:55:17 +0300

    toy conv2d example

---
+++ demo/naive_conv2d.cpp
@@ -0,0 +1,43 @@
+#include <print>
+#include <venus/memory/device.hpp>
+#include <venus/tensor/tensor.hpp>
+
+using namespace venus;
+
+auto conv2d(Tensor<float, Device::CPU, 2> &input,
+            Tensor<float, Device::CPU, 2> &kernel, std::size_t stride = 1)
+    -> Tensor<float, Device::CPU, 2> {
+  const auto [w, h] = input.shape();
+  const auto [kW, kH] = kernel.shape();
+
+  const auto outW = (w - kW) / stride + 1;
+  const auto outH = (h - kH) / stride + 1;
+
+  auto out = Tensor<float, Device::CPU, 2>(outW, outH);
+
+  for (std::size_t i{}; i < outW; ++i) {
+    for (std::size_t j{}; j < outH; ++j) {
+      for (std::size_t k1{}; k1 < kW; ++k1) {
+        for (std::size_t k2{}; k2 < kH; ++k2) {
+          out[i, j] += kernel[kW - 1 - k1, kH - 1 - k2] *
+                       input[i * stride + k1, j * stride + k2];
+        }
+      }
+    }
+  }
+
+  return out;
+}
+
+auto main() -> int {
+  auto img = Tensor<float, Device::CPU, 2>(5, 5);
+  img.fill(1.0F);
+
+  auto kernel =
+      Tensor<float, Device::CPU, 2>{{-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}};
+
+  auto res = conv2d(img, kernel);
+  assert(res.shape() == Shape(3, 3));
+
+  std::println("{}", res);
+}
