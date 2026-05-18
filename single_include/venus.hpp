f5c70701316f7cb0bffc6318f4b2e3fa8d8fd7c0
AndreiMoraru123 <81184255+AndreiMoraru123@users.noreply.github.com>
2026-05-18 13:08:48 +0000

    Update single header with latest code

--- single_include/venus.hpp
+++ single_include/venus.hpp
@@ -684,6 +684,18 @@ static constexpr bool HasNonTrivialPolicy =
 
 // =============================================================
 } // namespace venus
+#ifdef VENUS_INTERPRETER
+#include <iostream>
+namespace venus {
+template <typename T, Device D, std::size_t Rank>
+void __clang_repl__Display__(const Tensor<T, D, Rank> &t) {
+  std::cout << t << "\n";
+}
+template <std::size_t Rank> void __clang_repl__Display__(const Shape<Rank> &s) {
+  std::cout << s << "\n";
+}
+} // namespace venus
+#endif
 
 #include <algorithm>
 #include <concepts>
