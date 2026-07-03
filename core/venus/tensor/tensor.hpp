028a12fce31f5577de671bc20c3e69efc3230530
AndreiMoraru123 <andreimoraru1999123@gmail.com>
2026-07-03 12:41:40 +0300

    formatting

--- core/venus/tensor/tensor.hpp
+++ core/venus/tensor/tensor.hpp
@@ -392,7 +392,7 @@ public:
     }
   }
 
-  static auto eye(const Shape<rank>& shape) {
+  static auto eye(const Shape<rank> &shape) {
     auto tensor = Tensor(shape);
     tensor.eye();
     return tensor;
