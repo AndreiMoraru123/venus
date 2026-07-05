d22d6a176dbec251583c3c7c6d8b47e38c4ef2b0
AndreiMoraru123 <81184255+AndreiMoraru123@users.noreply.github.com>
2026-07-05 07:16:37 +0000

    Update single header with latest code

--- single_include/venus.hpp
+++ single_include/venus.hpp
@@ -2317,7 +2317,7 @@ public:
     return m_shape.count();
   }
 
-  auto data(this auto &&self) -> decltype(auto) {
+  auto data(this auto &&self) {
     return std::forward<decltype(self)>(self).m_mem.ptr();
   }
 
@@ -2445,9 +2445,9 @@ public:
     return LowLevelAccess<Self>(self);
   }
 
-  [[nodiscard]] constexpr auto size() const -> std::size_t { return 1; }
+  [[nodiscard]] constexpr auto size() const { return 1; }
 
-  auto data(this auto &&self) -> decltype(auto) {
+  auto data(this auto &&self) {
     return std::forward<decltype(self)>(self).m_mem.ptr();
   }
 
